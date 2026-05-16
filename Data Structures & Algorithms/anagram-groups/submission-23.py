c08053f820020d897ff34342b16fa0d48f9c18f8
TomerWeissman <156852980+TomerWeissman@users.noreply.github.com>
2026-05-16 10:51:14 +0300

    Add: anagram-groups - submission-23

---
+++ Data Structures & Algorithms/anagram-groups/submission-23.py
@@ -0,0 +1,16 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        
+        hashmap = defaultdict(list)
+        for s in strs:
+            mapper = [0]*26
+
+            for c in s:
+                mapper[ord(c) - ord('a')] += 1
+            
+            hashmap[tuple(mapper)].append(s)
+        
+        return list(hashmap.values())
+            
+
+        
