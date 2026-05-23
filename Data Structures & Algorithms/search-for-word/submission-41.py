8bdf7fcbc8b172db8b165a552ee12846467a2b0a
TomerWeissman <156852980+TomerWeissman@users.noreply.github.com>
2026-05-23 11:35:02 +0300

    Add: search-for-word - submission-41

---
+++ Data Structures & Algorithms/search-for-word/submission-41.py
@@ -0,0 +1,60 @@
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        
+        #create visit set
+        visit = set()
+        count = 0
+        neighbors = [(0, 1), (0, -1), (-1, 0), (1, 0)]
+
+        #DFS
+        def dfs(count, r, c):
+            #positive basecase:
+                ##length of path is eqaul to word
+            
+            if count == len(word):
+                return True
+
+            #basecases:
+                #if letter is not equal
+                #if off board
+                #if going back to prev
+            
+            if (
+                min(r,c) < 0 or
+                r == len(board) or
+                c == len(board[0]) or
+                (r,c) in visit or
+                word[count] != board[r][c]
+                ):
+                return
+            
+            #add to visit
+            visit.add((r,c))
+
+            #add letter to count
+            count += 1
+
+            #loop though neighbors with dfs
+            for dr, dc in neighbors:
+                row = r + dr
+                col = c + dc
+
+                if dfs(count, row, col):
+                    return True
+
+            #remove from visit
+            visit.remove((r,c))
+            #remove count
+            count -= 1
+        
+        #loop through all letters, if first, start dfs
+
+        for row in range(len(board)):
+            for col in range(len(board[0])):
+                if board[row][col] == word[0]:
+                    if dfs(0, row, col):
+                        return True
+            
+        return False
+
+
